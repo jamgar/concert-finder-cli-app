53ff0716495fd3ab76286ed1618c207ac6500e48
James Garcia <jgarcia@garciajames.com>
2017-05-17 16:16:11 +0000

    Add print_concerts method to cli.rb

--- lib/concert_finder_cli_app/cli.rb
+++ lib/concert_finder_cli_app/cli.rb
@@ -2,15 +2,22 @@ class ConcertFinderCliApp::CLI
 
   def call
     puts "Welcome to Concert Finder!"
+    puts ""
+    puts "Choose a city to view upcoming concerts:"
     print_cities
+    input = gets.strip
+
   end
 
   def print_cities
-    puts ""
     cities = ConcertFinderCliApp::Scraper.get_cities
-    puts "Choose a city to view upcoming concerts:"
     cities.each_with_index { |city, index| puts "#{index + 1}. #{city.name}" }
-    input = gets.strip
+  end
+
+  def print_concerts(index)
+    cities = ConcertFinderCliApp::Scraper.get_cities
+    concerts = ConcertFinderCliApp::Scraper.get_concerts(cities[index -1])
+    
   end
 
 end
--- lib/concert_finder_cli_app/scraper.rb
+++ lib/concert_finder_cli_app/scraper.rb
@@ -15,7 +15,7 @@ class ConcertFinderCliApp::Scraper
   end
 
   def self.scrape_city_page(city)
-    # city_page = Nokogiri::HTML(open("https://www.vividseats.com/region/#{city}/concert-tickets.html"))
+    city_page = Nokogiri::HTML(open(city.url))
 
     concerts = []
 
