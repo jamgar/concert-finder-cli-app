cc4e44a473b29e467c136ec0639b82cf5cad5027
James Garcia <jgarcia@garciajames.com>
2017-05-17 19:01:43 +0000

    Get date for concert

--- lib/concert_finder_cli_app/concert.rb
+++ lib/concert_finder_cli_app/concert.rb
@@ -1,3 +1,10 @@
 class ConcertFinderCliApp::Concert
+  attr_accessor :artist, :date, :location
+
+  @@all = []
+
+  def initialize
+    @@all << self
+  end
 
 end
--- lib/concert_finder_cli_app/scraper.rb
+++ lib/concert_finder_cli_app/scraper.rb
@@ -4,10 +4,7 @@ class ConcertFinderCliApp::Scraper
   def self.get_cities
     cities = Nokogiri::HTML(open("https://www.songkick.com/session/filter_metro_area"))
 
-    city_names = []
-
     cities.css("div.popular-metro-areas ol:first-child li").each_with_index do |item, index|
-      # url = item.css("a").attribute("href").value
       City.new(item.css("a").text, BASE_PATH + item.css("a").attribute("href").value) unless index == 0
     end
     City.all
@@ -16,14 +13,15 @@ class ConcertFinderCliApp::Scraper
   def self.get_concerts(city)
     city_page = Nokogiri::HTML(open(city.url))
 
-    concerts = []
-
     city_page.css("div.events-summary ul.event-listings li").each do |concert|
       if concert.css("p.artists strong").text != ""
-        concerts << concert.css("p.artists strong").text
+        # date = concert
+        date = concert.css("time").attribute("datetime").value
+        binding.pry
+        artist = concert.css("p.artists strong").text
+        Concert.new
       end
     end
-    binding.pry
   end
 
 end
